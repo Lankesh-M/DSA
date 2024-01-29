8fc0d4719ed1861c7f221ff5135ee4801db39975
Lankesh M <125749885+Lankesh-M@users.noreply.github.com>
2024-01-29 22:46:17 +0530

    Update 189.RotateArray.java

--- 189.RotateArray.java
+++ 189.RotateArray.java
@@ -1,3 +1,4 @@
+//Brute Force
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
@@ -13,5 +14,23 @@ class Solution {
         for(int j = 0; j < k; j++)
             nums[j] = temp[j];
 
+    }
+}
+//Optimal Solution
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1 - k);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end){
+        while(start <= end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
